87c9e9b2cb990400853dcedc219aee50a5cd9beb
mizzz（ずーみー） <mizzz._.dev@outlook.jp>
2026-07-23 08:04:39 +0900

    refactor(quote): Plugin APIのexportを整理

--- plugins/quote/src/index.ts
+++ plugins/quote/src/index.ts
@@ -1,2 +1,34 @@
-export { QuotePlugin } from './plugin.js';
-export { QuotePlugin as default } from './plugin.js';
+export { quotePlugin } from './plugin.js';
+export { quotePlugin as default } from './plugin.js';
+export { quoteManifest } from './manifest.js';
+export {
+  DEFAULT_QUOTE_CONFIG,
+  MAX_QUOTE_LENGTH,
+  MAX_QUOTE_TAG_LENGTH,
+  MAX_QUOTE_TAGS,
+  QuoteValidationError,
+  normalizeQuoteConfig,
+  normalizeOptionalText,
+  parseQuoteTags,
+  validateQuoteText,
+} from './config.js';
+export {
+  createQuote,
+  deleteQuote,
+  getQuoteByNumber,
+  getRandomQuote,
+  listQuotes,
+  updateQuote,
+} from './service.js';
+export type {
+  CreateQuoteInput,
+  DeleteQuoteInput,
+  ListQuotesInput,
+  ListQuotesResult,
+  QuoteOperationSource,
+  QuotePrismaClient,
+  QuoteRecord,
+  QuoteTransactionClient,
+  UpdateQuoteInput,
+} from './service.js';
+export type { QuoteConfig } from './config.js';
