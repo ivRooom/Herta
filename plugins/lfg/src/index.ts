2798e0555fb4e1cbee449ea3a3d93ab3a1be4bc1
mizzz（ずーみー）/いゔる。 <mizzz._.dev@outlook.jp>
2026-07-29 11:35:04 +0900

    feat(lfg): Discord表示生成を公開

--- plugins/lfg/src/index.ts
+++ plugins/lfg/src/index.ts
@@ -2,4 +2,5 @@ export { lfgPlugin } from './plugin.js';
 export { lfgPlugin as default } from './plugin.js';
 export * from './component-id.js';
 export * from './config.js';
+export * from './presentation.js';
 export * from './service.js';
