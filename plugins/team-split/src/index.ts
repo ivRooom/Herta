9ede02782f451f82b60761bb2d7f72219f4f1591
mizzz（ずーみー）/いゔる。 <mizzz._.dev@outlook.jp>
2026-07-29 15:20:06 +0900

    feat(team-split): Plugin RuntimeとServiceを公開

--- plugins/team-split/src/index.ts
+++ plugins/team-split/src/index.ts
@@ -1,2 +1,25 @@
-export { TeamSplitPlugin } from './plugin.js';
-export { TeamSplitPlugin as default } from './plugin.js';
+export { teamSplitPlugin } from './plugin.js';
+export { teamSplitPlugin as default } from './plugin.js';
+export { teamSplitManifest } from './manifest.js';
+export {
+  TEAM_SPLIT_DEFAULTS,
+  TeamSplitValidationError,
+  normalizeParticipantScore,
+  normalizeTeamSplitConfig,
+  normalizeTeamSplitSessionInput,
+} from './config.js';
+export {
+  createTeamSplitComponentId,
+  parseTeamSplitComponentId,
+} from './component-id.js';
+export {
+  buildTeamSplitDiscordMessage,
+  buildTeamSplitInteractionMessage,
+  formatTeamSplitSessionText,
+} from './presentation.js';
+export {
+  createTeamSplitMessageNonce,
+  deriveTeamSplitSeedHash,
+  splitTeamMembers,
+} from './split.js';
+export * from './service.js';
