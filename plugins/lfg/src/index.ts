a921b0747adf13a9639fe282e44c91bc8b4ed1a2
mizzz（ずーみー）/いゔる。 <mizzz._.dev@outlook.jp>
2026-07-29 11:29:45 +0900

    feat(lfg): v1 RuntimeとServiceを公開

--- plugins/lfg/src/index.ts
+++ plugins/lfg/src/index.ts
@@ -1,2 +1,5 @@
-export { LfgPlugin } from './plugin.js';
-export { LfgPlugin as default } from './plugin.js';
+export { lfgPlugin } from './plugin.js';
+export { lfgPlugin as default } from './plugin.js';
+export * from './component-id.js';
+export * from './config.js';
+export * from './service.js';
